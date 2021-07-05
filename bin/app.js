44a3872082c5078154839fc2d73647627e791e3a
Bryan Eduria <eduria.bryan.96@gmail.com>
2021-07-06 00:37:22 +0800

    options - wip

--- bin/app.js
+++ bin/app.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("Hello World!");
+const yargs = require('yargs')
+
+const options = yargs.usage('Usage: -d <domain>').option('d', {
+  alias: 'domain',
+  describe: 'Target domain',
+  type: 'string',
+  demandOption: true
+}).argv
+
+console.log(`Looking for the subdomains of ${options.domain} ...`)
